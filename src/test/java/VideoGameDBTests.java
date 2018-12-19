a367873d9b696b627d2c2f146879a27cc82d62b2
DESKTOP-DTRO1V7\Mateusz <drozdz91@interia.pl>
2018-12-19 22:33:24 +0100

    Added POST test to VideoGameDBTests.

--- src/test/java/VideoGameDBTests.java
+++ src/test/java/VideoGameDBTests.java
@@ -12,10 +12,31 @@ public class VideoGameDBTests extends TestConfig {
                 spec(videoGame_requestSpec).
                 log().
                 all().
-         when().get(EndPoint.VIDEOGAMES).
-         then().
+        when().
+                get(EndPoint.VIDEOGAMES).
+        then().
                 spec(responseSpec).
                 log().
                 all();
+    }
+
+    @Test
+    public void createNewGameByJSON() {
+
+        String gameBodyJson = "{\n" +
+                "  \"id\": 15,\n" +
+                "  \"name\": \"MyNewGame\",\n" +
+                "  \"releaseDate\": \"2018-12-19T20:45:20.180Z\",\n" +
+                "  \"reviewScore\": 50,\n" +
+                "  \"category\": \"Driving\",\n" +
+                "  \"rating\": \"Mature\"\n" +
+                "}";
+
+        given().
+                spec(videoGame_requestSpec).
+                body(gameBodyJson).
+        when().
+                post(EndPoint.VIDEOGAMES).
+        then();
     }
 }
