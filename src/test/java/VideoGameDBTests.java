37c1bca8a02b7d98efe6b0ab8dbd8fab450a568f
DESKTOP-DTRO1V7\Mateusz <drozdz91@interia.pl>
2018-12-20 23:06:28 +0100

    Added 3 tests: CreateGameByXml, updateGame and deleteGame.

--- src/test/java/VideoGameDBTests.java
+++ src/test/java/VideoGameDBTests.java
@@ -15,7 +15,6 @@ public class VideoGameDBTests extends TestConfig {
         when().
                 get(EndPoint.VIDEOGAMES).
         then().
-                spec(responseSpec).
                 log().
                 all();
     }
@@ -24,7 +23,7 @@ public class VideoGameDBTests extends TestConfig {
     public void createNewGameByJSON() {
 
         String gameBodyJson = "{\n" +
-                "  \"id\": 15,\n" +
+                "  \"id\": 11,\n" +
                 "  \"name\": \"MyNewGame\",\n" +
                 "  \"releaseDate\": \"2018-12-19T20:45:20.180Z\",\n" +
                 "  \"reviewScore\": 50,\n" +
@@ -37,6 +36,53 @@ public class VideoGameDBTests extends TestConfig {
                 body(gameBodyJson).
         when().
                 post(EndPoint.VIDEOGAMES).
+        then();
+    }
+
+    @Test
+    public void createNewGameByXML() {
+
+        String gameBodyXml = "<videoGame category=\"Shooter\" rating=\"Universal\">\n" +
+                "    <id>12</id>\n" +
+                "    <name>Resident Evil 7</name>\n" +
+                "    <releaseDate>2005-10-01T00:00:00+02:00</releaseDate>\n" +
+                "    <reviewScore>75</reviewScore>\n" +
+                "  </videoGame>";
+
+        given().
+                spec(videoGame_requestSpec).
+                body(gameBodyXml).
+        when().
+                post(EndPoint.VIDEOGAMES).
+        then();
+    }
+
+    @Test
+    public void updateGame() {
+
+        String gameBodyJson = "{\n" +
+                "  \"id\": 11,\n" +
+                "  \"name\": \"MyUpdatedGame\",\n" +
+                "  \"releaseDate\": \"2018-12-19T20:45:20.180Z\",\n" +
+                "  \"reviewScore\": 99,\n" +
+                "  \"category\": \"Driving\",\n" +
+                "  \"rating\": \"Mature\"\n" +
+                "}";
+
+        given().
+                spec(videoGame_requestSpec).
+                body(gameBodyJson).
+        when().
+                put("/videogames/11").
+        then();
+    }
+
+    @Test
+    public void deleteGame() {
+        given().
+                spec(videoGame_requestSpec).
+        when().
+                delete("/videogames/11").
         then();
     }
 }
