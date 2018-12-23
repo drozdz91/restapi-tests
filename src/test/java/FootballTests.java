8ab1805e8d20aa6fda50c261c58552cea00282b2
DESKTOP-DTRO1V7\Mateusz <drozdz91@interia.pl>
2018-12-23 19:13:23 +0100

    Added new FootballTests.

--- src/test/java/FootballTests.java
+++ src/test/java/FootballTests.java
@@ -1,6 +1,11 @@
 import config.EndPoint;
 import config.TestConfig;
+import io.restassured.http.ContentType;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -37,5 +42,67 @@ public class FootballTests extends TestConfig {
                 get(EndPoint.TEAMS_FOR_ONE_COMPETITION).
         then().
                 body("teams.name[0]", equalTo("VVV Venlo"));
+    }
+
+    @Test
+    public void getAllTeamData_DoCheckFirst() {
+        Response response =
+        given().
+                spec(football_requestSpec).
+                pathParam("competitionId", 2003).
+        when().
+                get(EndPoint.TEAMS_FOR_ONE_COMPETITION).
+        then().
+                contentType(ContentType.JSON).
+                extract().response();
+
+        String jsonResponseAsString = response.asString();
+    }
+
+    @Test
+    public void extractHeaders() {
+        Response response =
+        given().
+                spec(football_requestSpec).
+                pathParam("competitionId", 2003).
+        when().
+                get(EndPoint.TEAMS_FOR_ONE_COMPETITION).
+        then().
+                contentType(ContentType.JSON).
+                extract().response();
+
+        Headers headers = response.getHeaders();
+    }
+
+    @Test
+    public void extractFirstTeamName() {
+        String firstTeamName =
+        given().
+                spec(football_requestSpec).
+                pathParam("competitionId", 2003).
+        when().
+                get(EndPoint.TEAMS_FOR_ONE_COMPETITION).
+                jsonPath().getString("teams.name[0]");
+
+        System.out.println(firstTeamName);
+    }
+
+    @Test
+    public void extractAllTeamNames() {
+        Response response =
+        given().
+                spec(football_requestSpec).
+                pathParam("competitionId", 2003).
+        when().
+                get(EndPoint.TEAMS_FOR_ONE_COMPETITION).
+        then().
+                contentType(ContentType.JSON).
+                extract().response();
+
+        List<String> teamNames = response.path("teams.name");
+
+        for (String teamName: teamNames) {
+            System.out.println(teamName);
+        }
     }
 }
--- src/test/java/config/EndPoint.java
+++ src/test/java/config/EndPoint.java
@@ -2,9 +2,11 @@ package config;
 
 public interface EndPoint {
 
+    // Endpoints for VideoGameDB
     String VIDEOGAMES = "/videogames";
     String SINGLE_VIDEOGAME = "/videogames/{videoGameId}";
 
+    // Endpoints for Football API
     String COMPETITIONS_PL_MATCHES = "competitions/PL/matches";
     String TEAMS_FOR_ONE_COMPETITION = "competitions/{competitionId}/teams";
 }
